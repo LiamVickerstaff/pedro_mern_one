378b2a725d46cf41c16659bd52ee2d72cd4fdb77
Liam Vickerstaff <liamvickerstaff050@gmail.com>
2024-10-08 22:58:51 +1100

    Add Financial Record Form And List Components, React Router Install And Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/auth";
+import Dashboard from "./pages/dashboard";
 
 function App() {
-
   return (
-    <>
-      <h1>PedroTechFinance</h1>
-    </>
-  )
+    <Router>
+      <div className={styles.appContainer}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/dashboard/financial-record-list.tsx
@@ -0,0 +1,3 @@
+export default function FinancialRecordList() {
+  return <div>Record List</div>;
+}
